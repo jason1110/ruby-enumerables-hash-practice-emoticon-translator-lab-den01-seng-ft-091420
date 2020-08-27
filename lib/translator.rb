0b6e8f06ad6ebaada284ad719227274c58866c68
Jason Easterly <jason.easterly11@gmail.com>
2020-08-27 22:59:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,7 @@ end
 def get_english_meaning(file_path,j_emote)
   library = load_library(file_path).each do |meaning, emote|
     if j_emote == emote
-      puts meaning
+      meaning
   binding.pry
     end
   end
