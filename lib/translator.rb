046161972316cf3aceae89ff2c06ec85be4b90bc
Jason Easterly <jason.easterly11@gmail.com>
2020-08-27 15:42:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,7 @@ new_hash = {}
 get_file.each do |key, value|
     new_hash = {key => {:english => value[0], :japanese => value[1]}}
   end
-new_hash = make_hash  
+new_hash
 binding.pry
 end
 #def get_japanese_emoticon(load_library,eng_emote)
