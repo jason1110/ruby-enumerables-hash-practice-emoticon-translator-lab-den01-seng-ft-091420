622768bf1e92a183e6409924bd61abacc98bec9c
Jason Easterly <jason.easterly11@gmail.com>
2020-08-27 22:15:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ def load_library(path)
 get_file = YAML.load_file(path)
 new_hash = {}
   get_file.each do |key, value|
-    new_hash[key] = {}
+    new_hash[key.to_sym] = {}
     new_hash[key][:english] = value[0]
     new_hash[key][:japanese] = value[1]
   end
