2f95231ebb8268bf3c7b902aa581f2a97f8c7348
Jason Easterly <jason.easterly11@gmail.com>
2020-08-27 03:07:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,14 +1,12 @@
 require 'yaml'
 require 'pry'
 
-def load_library
-get_file = YAML.load_file('lib/emoticons.yml')
-v1 = {}
-v2 = {}
-  make_hash = get_file.each_with_object({}) do |(key,value), new_hash|
-    v1 = value[0]
-    v2 =  value[1]
-    new_hash[key.to_sym] = {:english => v1, :japanese => v2}
+def load_library(file)
+get_file = YAML.load_file(file)
+new_hash = {}
+  make_hash = get_file.each do |key,value|
+    new_hash[key.to_sym][:english] = value[0]
+    new_hash[key.to_sym][:japanese] = value[1]
   end
 make_hash
 binding.pry
