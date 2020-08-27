77dd2e0d2e8d56036330c50911d633c1d19476a8
Jason Easterly <jason.easterly11@gmail.com>
2020-08-27 15:25:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,6 +6,7 @@ get_file = YAML.load_file(path)
 new_hash = {}
   get_file.each do |key, value|
     new_hash = {key => {:english => value[0], :japanese => value[1]}}
+    new_hash
   end
 new_hash
 binding.pry
