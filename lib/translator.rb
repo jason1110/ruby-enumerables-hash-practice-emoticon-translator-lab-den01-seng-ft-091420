2da7274602d4b46273348134cd18c5f2c4c00177
Jason Easterly <jason.easterly11@gmail.com>
2020-08-28 02:03:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,7 @@ new_hash
 end
 def get_japanese_emoticon(file_path,eng_emote)
 japanese_emoticon = "sorry dont know what that means"
-  load_library(file_path).each_value do |english, japanese|
+  load_library(file_path).each_value do |value|
     binding.pry
     if english == japanese
      japanese_emoticon = japanese
